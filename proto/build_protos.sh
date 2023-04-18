36e396ea25ac2725bcc50bab31af1e9f5b8b3d8a
David Sisson <EpsilonPrime@users.noreply.github.com>
2023-04-17 23:20:54 -0700

    Add some useful comments.

--- proto/build_protos.sh
+++ proto/build_protos.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Assume we're running from the proto directory (where this script is located).
+
 # Since we don't control where the repo has been checked out we first create the protobufs and then move them into place.
 protoc --go_out=. --proto_path=../third-party/substrait/proto ../third-party/substrait/proto/substrait/*proto
 protoc --go_out=. --proto_path=../third-party/substrait/proto ../third-party/substrait/proto/substrait/extensions/*proto
@@ -7,6 +9,8 @@ protoc --go_out=. --proto_path=../third-party/substrait/proto ../third-party/sub
 # Move them.
 mv github.com/substrait-io/substrait-go/proto/*.pb.go .
 mv github.com/substrait-io/substrait-go/proto/extensions/*.pb.go extensions/
+
+# Clean up the leftover directories but use rmdir so we don't accidentally delete something unexpectedly.
 rmdir github.com/substrait-io/substrait-go/proto/extensions
 rmdir github.com/substrait-io/substrait-go/proto
 rmdir github.com/substrait-io/substrait-go
